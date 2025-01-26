9574edb8ece29d55ec9112be81535d17bcea2760
Njaka ANDRIAMAHENINA <a6njaka@gmail.com>
2025-01-26 10:23:22 +0300

    Create lib_multiplication_par_8_ver.py

---
+++ lib_multiplication_par_8_ver.py
@@ -0,0 +1,14 @@
+import random
+import lib_template_two_num_ver
+
+
+def get_image_data(data):
+    level = data["level"]
+    if level <= 1:
+        level = 1
+    elif level > 9:
+        level = 9
+    a = random.randint(10 ** (level-1), 10 ** level)
+    b = 8
+
+    return lib_template_two_num_ver.operation2number_vertical(a, b, "*")
