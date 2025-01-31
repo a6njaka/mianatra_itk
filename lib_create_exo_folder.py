a879d0637351b2a154076f8ffdc583b27637a676
Njaka ANDRIAMAHENINA <a6njaka@gmail.com>
2025-01-31 05:30:00 +0300

    Update lib_create_exo_folder.py

--- lib_create_exo_folder.py
+++ lib_create_exo_folder.py
@@ -5,7 +5,7 @@ exo_dir = r"D:\Njaka_Project\Njaka_Dev_Itk\bin\Mianatra2\images"
 # exo_dir = r"C:\Users\NJAKA\Desktop\itokiana_png"
 
 
-exo_name = f"nombre_plus_petit"
+exo_name = f"nombre_plus_petit_max10"
 exo_path = os.path.join(exo_dir, exo_name)
 config_json_path = os.path.join(exo_path, 'config.json')
 
@@ -13,14 +13,14 @@ if not os.path.isdir(exo_path):
     os.mkdir(exo_path)
 
 config = {
-    "min": 10,
-    "max": 15,
+    "min": 20,
+    "max": 25,
     "level": 3,
     "rand": True,
     "type": "entry",
     "case sensitive": True,
-    "library": f"lib_smallest_number_under_100",
-    "comment": f"Nombre le plus petit"
+    "library": f"lib_smallest_number_under_10",
+    "comment": f"Nombre le plus petit inférieur à 10"
 }
 with open(config_json_path, 'w') as outfile:
     json.dump(config, outfile, indent=3)
