840bfe9ad570a18e1216ccf5957d6292ebbb27ab
Njaka ANDRIAMAHENINA <a6njaka@gmail.com>
2025-02-05 16:01:53 +0300

    Update lib_compare_number.py

--- lib_compare_number.py
+++ lib_compare_number.py
@@ -32,16 +32,37 @@ def get_image_data(data):
     total_text_width = a_width + b_width + compare_width
 
     # Calculate starting position to center the text
-    start_x = (width - total_text_width) // 2
+    w_separator = 20
+    start_x = (width - total_text_width - w_separator * 2) // 2
     text_y = (height - draw1.textbbox((0, 0), f"{a}", font=font)[3]) // 2
 
     x_a = start_x
-    x_compare = start_x + a_width
-    x_b = x_compare + compare_width
+    x_compare = start_x + a_width + w_separator
+    x_b = x_compare + compare_width + w_separator
 
-    draw1.text((15, 20), f"{a}", fill=(255, 0, 0), font=font)
-    draw1.text((15, 20), f"...", fill=(255, 0, 0), font=font)
-    draw1.text((15, 20), f"{b}", fill=(255, 0, 0), font=font)
+    draw1.text((x_a, text_y), f"{a}", fill=(0, 0, 0), font=font)
+    draw1.text((x_compare, text_y), f"...", fill=(255, 0, 0), font=font)
+    draw1.text((x_b, text_y), f"{b}", fill=(0, 0, 0), font=font)
+
+    sign = ""
+    answer = re.compile(rf"^\s*$")
+    if a<b:
+        sign = "<"
+        answer = re.compile(rf"^\s*C1\s*$")
+    elif a == b:
+        sign = "="
+        answer = re.compile(rf"^\s*C2\s*$")
+    else:
+        sign = ">"
+        answer = re.compile(rf"^\s*C3\s*$")
+
+    draw1.text((x_a, text_y), f"{a}", fill=(0, 0, 0), font=font)
+    draw1.text((x_compare, text_y), f"...", fill=(255, 0, 0), font=font)
+    draw1.text((x_b, text_y), f"{b}", fill=(0, 0, 0), font=font)
+
+    draw2.text((x_a, text_y), f"{a}", fill=(0, 0, 0), font=font)
+    draw2.text((x_compare, text_y), sign, fill=(255, 0, 0), font=font)
+    draw2.text((x_b, text_y), f"{b}", fill=(0, 0, 0), font=font)
 
     pil_image1 = image1.convert('RGB')
     pil_image2 = image2.convert('RGB')
@@ -49,12 +70,10 @@ def get_image_data(data):
     image_data1 = pil_image1.tobytes()
     image_data2 = pil_image2.tobytes()
 
-    answer = re.compile(rf"^\s*C2\s*$")
-    text = "Andrana"
-
+    text = f"{a} vs {b}"
     choices = [image_text_center1("<"), image_text_center1("="), image_text_center1(">")]
 
-    return image_text_center2("ANDRIAMAHENINA Njaka"), image_data2, choices, answer, text
+    return image_data1, image_data2, choices, answer, text
     # return r"D:\Njaka_Project\Njaka_Dev_Itk\bin\Mianatra2\images\yes_no_2\A1.png", r"D:\Njaka_Project\Njaka_Dev_Itk\bin\Mianatra2\images\yes_no_2\A2.png", choices, answer, text
 
 
@@ -100,7 +119,6 @@ def image_text_center2(text, size=(854, 480), font_size=50):
 
     image_data = pil_image.tobytes()
     return image_data
-
 
 # image_text_center1("<")
 # image_text_center2("ANDRIAMAHENINA Njaka")
