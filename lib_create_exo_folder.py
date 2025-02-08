edee977e938980149a4461656a78ab3bf06fd555
Njaka ANDRIAMAHENINA <a6njaka@gmail.com>
2025-02-08 21:31:10 +0300

    Update lib_create_exo_folder.py

--- lib_create_exo_folder.py
+++ lib_create_exo_folder.py
@@ -5,7 +5,7 @@ exo_dir = r"D:\Njaka_Project\Njaka_Dev_Itk\bin\Mianatra2\images"
 # exo_dir = r"C:\Users\NJAKA\Desktop\itokiana_png"
 
 
-exo_name = f"signe_de_comparaison"
+exo_name = f"montre_a_aiguilles"
 exo_path = os.path.join(exo_dir, exo_name)
 config_json_path = os.path.join(exo_path, 'config.json')
 
@@ -13,14 +13,14 @@ if not os.path.isdir(exo_path):
     os.mkdir(exo_path)
 
 config = {
-    "min": 20,
-    "max": 25,
+    "min": 10,
+    "max": 15,
     "level": 3,
     "rand": True,
     "type": "entry",
     "case sensitive": True,
-    "library": f"lib_compare_number",
-    "comment": f"Utilisation de signe de comparaison"
+    "library": f"lib_analog_clock",
+    "comment": f"Exercice montre à aiguilles"
 }
 with open(config_json_path, 'w') as outfile:
     json.dump(config, outfile, indent=3)
