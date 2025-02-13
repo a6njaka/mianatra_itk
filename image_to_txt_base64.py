0a17e2c048c3bf78c102692d3f5e8bc9f8e1518e
Njaka ANDRIAMAHENINA <a6njaka@gmail.com>
2025-02-13 18:08:48 +0300

    Create image_to_txt_base64.py

---
+++ image_to_txt_base64.py
@@ -0,0 +1,8 @@
+import base64
+
+
+image = r"C:\Users\NJAKA\Desktop\istockphoto-597276512-612x612 - Copie.jpg"
+with open(image, "rb") as image_file:
+    base64_string = base64.b64encode(image_file.read()).decode("utf-8")
+
+print(base64_string)
