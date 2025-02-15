7300fd042c3083624378d21d4cb32eb3b1be3dd3
Njaka ANDRIAMAHENINA <a6njaka@gmail.com>
2025-02-15 18:46:47 +0300

    Update lib_soustraction_2ch_ver.py

--- lib_soustraction_2ch_ver.py
+++ lib_soustraction_2ch_ver.py
@@ -6,9 +6,11 @@ def get_image_data(data):
     level = data["level"]
     if level <= 0:
         level = 1
-    elif level > 3:
-        level = 3
+    elif level > 4:
+        level = 4
     a = random.randint(10 ** level, 10 ** (level + 1))
-    b = random.randint(10 ** (level - 1), 10 ** level - 1)
+    b = random.randint(1, 10 ** (level + 1))
+    while a <= b:
+        b = random.randint(1, 10 ** (level + 1))
 
     return lib_template_two_num_ver.operation2number_vertical(a, b, "-")
