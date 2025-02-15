610b6aedcbe49a6a2929ece7e7a6bd4c2175d9af
Njaka ANDRIAMAHENINA <a6njaka@gmail.com>
2025-02-15 18:48:03 +0300

    Update image_to_txt_base64.py

--- image_to_txt_base64.py
+++ image_to_txt_base64.py
@@ -2,6 +2,7 @@ import base64
 
 
 image = r"C:\Users\NJAKA\Desktop\istockphoto-597276512-612x612 - Copie.jpg"
+image = r"C:\Users\NJAKA\Desktop\Montre_exo.png"
 with open(image, "rb") as image_file:
     base64_string = base64.b64encode(image_file.read()).decode("utf-8")
 
