01247f5cef3cf2fcb3edebf9613a24110fb8b5a6
Njaka ANDRIAMAHENINA <a6njaka@gmail.com>
2025-02-26 21:16:51 +0300

    Update create_exo_folder.py

--- create_exo_folder.py
+++ create_exo_folder.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 
 exo_dir = r"D:\Njaka_Project\Njaka_Dev_Itk\bin\Mianatra2\images"
 # exo_dir = r"C:\Users\NJAKA\Desktop\itokiana_png"
@@ -20,7 +21,8 @@ config = {
     "type": "entry",
     "case sensitive": False,
     "library": f"lib_object_count",
-    "comment": f"Manisa zavatra"
+    "comment": f"Manisa zavatra",
+    "creation date": datetime.now().strftime("%d/%m/%Y")
 }
 with open(config_json_path, 'w') as outfile:
     json.dump(config, outfile, indent=3)
