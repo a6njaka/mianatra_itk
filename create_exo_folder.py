4a35fc498ff2bc594030c5fc22e9167a040278f8
Njaka ANDRIAMAHENINA <a6njaka@gmail.com>
2025-02-26 08:17:45 +0300

    Create create_exo_folder.py

---
+++ create_exo_folder.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+exo_dir = r"D:\Njaka_Project\Njaka_Dev_Itk\bin\Mianatra2\images"
+# exo_dir = r"C:\Users\NJAKA\Desktop\itokiana_png"
+
+
+exo_name = f"count_object"
+exo_path = os.path.join(exo_dir, exo_name)
+config_json_path = os.path.join(exo_path, 'config.json')
+
+if not os.path.isdir(exo_path):
+    os.mkdir(exo_path)
+
+config = {
+    "min": 10,
+    "max": 15,
+    "level": 1,
+    "rand": True,
+    "type": "entry",
+    "case sensitive": False,
+    "library": f"lib_object_count",
+    "comment": f"Manisa zavatra"
+}
+with open(config_json_path, 'w') as outfile:
+    json.dump(config, outfile, indent=3)
+
+
