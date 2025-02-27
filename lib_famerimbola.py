6ba98f7c614bd9276bff3e99a8a17c9d62fffa06
Njaka ANDRIAMAHENINA <a6njaka@gmail.com>
2025-02-27 20:17:11 +0300

    Update lib_famerimbola.py

--- lib_famerimbola.py
+++ lib_famerimbola.py
@@ -8,6 +8,7 @@ import copy
 def get_image_data(data):
     level = data["level"]
     folder_path = data["folder_path"]
+    folder_vola = data["folder_path"]
     folder_path = os.path.join(folder_path, "source_images")
     all_image = []
     if os.path.isdir(folder_path):
@@ -21,12 +22,14 @@ def get_image_data(data):
                         "article": m.group(1),
                         "price": m.group(2)}
                     )
-        img1 = Image.new('RGB', (854, 480), color=(255, 255, 255))
+        width, height = 1200, 480
+
+        img1 = Image.new('RGB', (width, height), color=(255, 255, 255))
         draw1 = ImageDraw.Draw(img1)
         font_price = ImageFont.truetype('arial.ttf', 25)
         font_question_mark = ImageFont.truetype('arial.ttf', 25)
-        logo_width = 110
-        marge = 15
+        logo_width = 150
+        marge = 0
         n = len(all_image)
         i = 0
         final_price = 0
@@ -39,7 +42,7 @@ def get_image_data(data):
             level = 3
         if level == 1:
             index = random.randint(0, n - 1)
-            for _ in range(random.randint(2, 4)):
+            for _ in range(random.randint(1, 3)):
                 exo_image_list.append(all_image[index])
         elif level == 2:
             index1 = random.randint(0, n - 1)
@@ -72,7 +75,12 @@ def get_image_data(data):
                 exo_image_list.append(all_image[index3])
         m = len(exo_image_list)
         x0 = int((854 - (logo_width * m + marge * (m - 1))) / 2)
+
+        x0 = 580
         logo_y = 300
+        font_final_price = ImageFont.truetype('arial.ttf', 40)
+        draw1.text((x0, logo_y - 80), "-", font=font_final_price, fill='red')
+        x0 += 25
         for images in exo_image_list:
             max_article_number = 6
             a = random.randint(0, max_article_number)
@@ -93,23 +101,53 @@ def get_image_data(data):
             text_height = text_bbox[3] - text_bbox[1]  # Bottom - Top
             d = (logo_width - text_width) / 2
             if i > 0:
-                draw1.text((x0 - marge + (logo_width + marge) * i, logo_y - 60), "+", font=font_price, fill='black')
+                draw1.text((x0 - marge + (logo_width + marge) * i, logo_y - 60), "", font=font_price, fill='black')
             draw1.text((x0 + (logo_width + marge) * i + d, logo_y), price, font=font_price, fill='black')
             i += 1
 
-        draw1.text((x0 - marge + (logo_width + marge) * i, logo_y - 60), "= ?", font=font_price, fill='red')
+        draw1.text((x0 - marge + (logo_width + marge) * i, logo_y - 60), "", font=font_price, fill='red')
         font = ImageFont.truetype('arial.ttf', 25)
-        draw1.text((20, 10), "Hoatrinona ireo?", font=font, fill='black')
-        font_final_price = ImageFont.truetype('arial.ttf', 40)
+        draw1.text((20, 10), "Hoatrinona ny famerimbola?", font=font, fill='black')
+
+        # Insert vola image
+        vola_rehetra = [5000, 2000, 1000, 500]
+        tmp_vola = []
+        for v in vola_rehetra:
+            if v > final_price:
+                tmp_vola.append(v)
+        r = random.randint(0, len(tmp_vola)-1)
+        vola_noentina = tmp_vola[r]
+        vola_noentina_image = os.path.join(folder_vola, "vola", f"{vola_noentina}.jpg")
+        if os.path.isfile(vola_noentina_image):
+            vola_width = 500
+            img_vola = Image.open(vola_noentina_image)
+            aspect_ratio = img_vola.width / img_vola.height
+            logo_height = int(vola_width / aspect_ratio)
+            img_vola = img_vola.resize((vola_width, logo_height))
+            img1.paste(img_vola, (60, 100))
+        else:
+            print(vola_noentina_image)
+
         img2 = copy.deepcopy(img1)
         draw2 = ImageDraw.Draw(img2)
 
-        draw1.text((350, 400), "............ Ariary", font=font_final_price, fill='red')
-        draw2.text((350, 400), f"{final_price} Ariary", font=font_final_price, fill='red')
+        answer = f"{vola_noentina-final_price}"
+        draw1.text((350, 400), "Famerimbola = ............ Ariary", font=font_final_price, fill='blue')
+        draw2.text((350, 400), f"Famerimbola = {answer} Ariary", font=font_final_price, fill='blue')
 
         pil_image1 = img1.convert('RGB')
         pil_image2 = img2.convert('RGB')
 
-        answer = f"{final_price}"
+        pil_image1.save(r"C:\Users\NJAKA\Desktop\01.jpg")
+
+        print(answer)
         return pil_image1, pil_image2, [], [], re.compile(rf"^\s*{re.escape(answer)}\s*(Ar|Ariary)?$", re.IGNORECASE), f"{final_price_str}={answer}"
     return None, None, None, None, None
+
+
+# data0 = {
+#     "level": 1,
+#     "folder_path": r"C:\Users\NJAKA\Desktop\Raw images\exo"
+# }
+#
+# get_image_data(data0)
